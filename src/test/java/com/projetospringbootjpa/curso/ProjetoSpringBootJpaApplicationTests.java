fad42e0a324ee3107e23df47e80a783cfbeb7157
Bruno Gomes <brunosoges@gmail.com>
2021-06-03 16:32:34 -0300

    Projeto criado

---
+++ src/test/java/com/projetospringbootjpa/curso/ProjetoSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospringbootjpa.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
