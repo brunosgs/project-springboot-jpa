01ce5f983305e98768e34061985069c54cb22eaf
Bruno Gomes <brunosoges@gmail.com>
2021-06-08 14:34:51 -0300

    Entidade OrderItem. Associação muitos-para-muitos com dados extras - parte 2

--- src/main/java/com/projetospringbootjpa/curso/entities/Order.java
+++ src/main/java/com/projetospringbootjpa/curso/entities/Order.java
@@ -2,6 +2,8 @@ package com.projetospringbootjpa.curso.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -29,8 +32,10 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "id_client")
 	private User client;
-
 	private Integer orderStatus;
+
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
 	}
@@ -75,6 +80,10 @@ public class Order implements Serializable {
 		if (orderStatus != null) {
 			this.orderStatus = orderStatus.getCode();
 		}
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
 	}
 
 	@Override
