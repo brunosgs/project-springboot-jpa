ed5392a330744e14f5408a2a49a30eb49aaf225a
Bruno Gomes <brunosoges@gmail.com>
2021-06-09 08:29:18 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/projetospringbootjpa/curso/config/TestConfig.java
+++ src/main/java/com/projetospringbootjpa/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetospringbootjpa.curso.entities.Category;
 import com.projetospringbootjpa.curso.entities.Order;
 import com.projetospringbootjpa.curso.entities.OrderItem;
+import com.projetospringbootjpa.curso.entities.Payment;
 import com.projetospringbootjpa.curso.entities.Product;
 import com.projetospringbootjpa.curso.entities.User;
 import com.projetospringbootjpa.curso.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2021-06-20T21:53:07Z"), o1);
+
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 
 }
