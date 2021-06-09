8eb74b13f7bc3ff6199442942efc9950d19672a8
Bruno Gomes <brunosoges@gmail.com>
2021-06-09 08:45:14 -0300

    Métodos subtotal e total

--- src/main/java/com/projetospringbootjpa/curso/entities/Order.java
+++ src/main/java/com/projetospringbootjpa/curso/entities/Order.java
@@ -99,6 +99,16 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+		double sum = 0d;
+		
+		for (OrderItem orderItem : items) {
+			sum += orderItem.getSubTotal();
+		}
+		
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
